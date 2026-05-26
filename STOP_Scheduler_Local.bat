cb28e38a6277e11b616fc314d9825ecdb036baff
Vijay Krishna M <156293446+vijaykrishna68@users.noreply.github.com>
2026-05-26 11:13:58 +0530

    Delete STOP_Scheduler_Local.bat

--- STOP_Scheduler_Local.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-REM Kill whatever is listening on port 4000
-for /f "tokens=5" %%a in ('netstat -ano ^| findstr /r /c:":4000 .*LISTENING"') do (
-  taskkill /PID %%a /F
-)
-
-endlocal
